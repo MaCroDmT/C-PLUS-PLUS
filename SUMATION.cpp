328e2833875d63387c34b85339797c6405ebeeff
PROTTOY SAHA_AIUB <121964459+MaCroDmT@users.noreply.github.com>
2025-04-08 22:44:41 +0600

    Add files via upload

---
+++ SUMATION.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main()
+{
+    int n;
+    std::cout<<"PLEASE ENTER THE VALUE OF N: ";
+    std::cin>>n;
+    
+}
---
+++ matrixsum.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+int main()
+
+{
+    int matrix_1 [3][3];
+
+ for (int i=0; i<3;i++)
+{
+    for(int j=0; j<3;j++)
+    {
+        std::cin>>matrix_1 [i][j];
+    }
+    //std::cout<<matrix_1;
+}
+
+int matrix_2 [3][3];
+
+ for (int k=0; k<3;k++)
+{
+    for(int l=0; l<3;l++)
+    {
+        std::cin>>matrix_2 [k][l];
+    }
+    //std::cout<<matrix_1;
+}
+
+/*int matrix_3 [3][3];
+
+ 
+        int sum= matrix_2 [k][l]+matrix_1 [i][j];
+    }
+    //std::cout<<matrix_1; */
+}
+
+ 
+
+
